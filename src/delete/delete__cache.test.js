664f3ada908997b618306d09ef943100c14d5ffb
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2019-03-05 23:49:06 +0100

    Add more delete tests

---
+++ src/delete/delete__cache.test.js
@@ -0,0 +1,44 @@
+import test from "tape"
+import { createStore, combineReducers } from "redux"
+
+import { buildList } from ".."
+
+test("Delete - caching", t => {
+  // WHAT TO TEST
+  const todoList = buildList({
+    name: "DELETE-CACHE_TODOS",
+    cacheTTL: 1000,
+    methods: {
+      find: () => [{ id: 1, name: "lorem ipsum" }, { id: 2, name: "foo bar" }],
+      delete: id => ({
+        id,
+      }),
+    },
+  })
+
+  // Redux store
+  const store = createStore(
+    combineReducers({
+      [todoList.name]: todoList.reducer,
+    })
+  )
+
+  // Link lists's action to store's dispatch
+  const listFind = todoList.find(store.dispatch)
+  const listDelete = todoList.delete(store.dispatch)
+
+  listFind()
+    .then(() => listDelete(2))
+    .then(id => {
+      const todosSelector = todoList.selector(store.getState())
+
+      t.equals(id, 2, "list.delete resolves with element id")
+
+      t.deepEquals(
+        todosSelector.items(),
+        [{ id: 1, name: "lorem ipsum" }],
+        "element should be removed from items array"
+      )
+    })
+    .finally(() => t.end())
+})
