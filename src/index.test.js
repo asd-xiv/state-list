31e6297dff3172a5a00c3c2e4496cd6ed89591b7
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2019-04-16 11:57:12 +0200

    Add tests for .add and .clear

--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,11 @@
 import test from "tape"
 import { createStore, combineReducers } from "redux"
 
-import { buildCollection } from "."
+import { buildList } from "."
 
 test("List without API methods", t => {
   // WHAT TO TEST
-  const todoList = buildCollection({
+  const todoList = buildList({
     name: "TODOS",
     cache: 1,
     methods: {},
@@ -24,13 +24,16 @@ test("List without API methods", t => {
   const listUpdate = todoList.update(store.dispatch)
   const listDelete = todoList.delete(store.dispatch)
 
+  const listAdd = todoList.add(store.dispatch)
+  const listClear = todoList.clear(store.dispatch)
+
   t.equals(todoList.name, "TODOS", "New list created with unique name")
 
   t.throws(
     () => {
-      buildCollection({ name: "TODOS" })
+      buildList({ name: "TODOS" })
     },
-    /ReduxCollections: List with name "TODOS" already exists/,
+    /ReduxAllIsList: List with name "TODOS" already exists/,
     "Throw exception when creating a list with a duplicate name"
   )
 
@@ -68,7 +71,7 @@ test("List without API methods", t => {
     () => {
       listCreate({ id: 2 })
     },
-    /ReduxCollections: "TODOS"."create" should be a function, got "undefined"/,
+    /ReduxAllIsList: "TODOS"."create" should be a function, got "undefined"/,
     'Throw exception when calling "create" on list without methods'
   )
 
@@ -76,7 +79,7 @@ test("List without API methods", t => {
     () => {
       listFind()
     },
-    /ReduxCollections: "TODOS"."find" should be a function, got "undefined"/,
+    /ReduxAllIsList: "TODOS"."find" should be a function, got "undefined"/,
     'Throw exception when calling "find" on list without methods'
   )
 
@@ -84,7 +87,7 @@ test("List without API methods", t => {
     () => {
       listUpdate(1, { test: 2 })
     },
-    /ReduxCollections: "TODOS"."update" should be a function, got "undefined"/,
+    /ReduxAllIsList: "TODOS"."update" should be a function, got "undefined"/,
     'Throw exception when calling "update" on list without methods'
   )
 
@@ -92,9 +95,45 @@ test("List without API methods", t => {
     () => {
       listDelete(1, { test: 2 })
     },
-    /ReduxCollections: "TODOS"."delete" should be a function, got "undefined"/,
+    /ReduxAllIsList: "TODOS"."delete" should be a function, got "undefined"/,
     'Throw exception when calling "delete" on list without methods'
   )
 
-  t.end()
+  listAdd({ id: 1, foo: "bar" })
+    .then(result => {
+      const selector = todoList.selector(store.getState())
+
+      t.deepEquals(
+        { id: 1, foo: "bar" },
+        result,
+        "Builtin .add should resolve with passed item"
+      )
+
+      t.deepEquals(
+        selector.items(),
+        [result],
+        "Builtin .add should add item to state"
+      )
+    })
+    .then(() => listClear())
+    .then(() => {
+      const selector = todoList.selector(store.getState())
+
+      t.deepEquals(
+        selector.items(),
+        [],
+        "Builtin .clear should remove all items from state"
+      )
+    })
+    .then(() => listAdd([{ id: 1, foo: "bar" }, { id: 2, foo: "bar2" }]))
+    .then(() => {
+      const selector = todoList.selector(store.getState())
+
+      t.deepEquals(
+        selector.items(),
+        [{ id: 1, foo: "bar" }, { id: 2, foo: "bar2" }],
+        "Builtin .add should add items to state"
+      )
+    })
+    .then(() => t.end())
 })
