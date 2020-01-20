c53b935d375e1831c7a2dd6f958ad69bff0dd869
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2020-01-20 22:19:40 +0100

    refactor: Remove unnecessary curry

--- src/use-list.js
+++ src/use-list.js
@@ -5,20 +5,19 @@ import { useListSelector } from "./use-list-selector"
 /**
  * Custom hook for easy interfacing Redux List
  *
- * @param {Object}    list      Any Redux List object
- * @param {Function}  dispatch  Redux store dispatch
- * @param {Object}    state     Redux store full state
+ * @param {Object}   list     Any Redux List object
+ * @param {Function} dispatch Redux dispatch
  *
  * @returns {{selector, create, read, readOne, update, remove, clear}}
  */
 const useList = (list, dispatch) => ({
   selector: useListSelector(list.name),
-  create: (...args) => list.create(dispatch)(...args),
-  read: (...args) => list.read(dispatch)(...args),
-  readOne: (...args) => list.readOne(dispatch)(...args),
-  update: (...args) => list.update(dispatch)(...args),
-  remove: (...args) => list.remove(dispatch)(...args),
-  clear: (...args) => list.clear(dispatch)(...args),
+  create: (...args) => list.create(dispatch, ...args),
+  read: (...args) => list.read(dispatch, ...args),
+  readOne: (...args) => list.readOne(dispatch, ...args),
+  update: (...args) => list.update(dispatch, ...args),
+  remove: (...args) => list.remove(dispatch, ...args),
+  clear: (...args) => list.clear(dispatch, ...args),
 })
 
 export { useList }
