a5ab21b49cb8a5237706e356a08f4bcfabc65154
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2020-02-07 13:37:09 +0100

    feat: Update selectors to proxy values from state, not create new instances. You can now benefit from useEffect, useCallback etc.

--- src/use-list.js
+++ src/use-list.js
@@ -1,6 +1,6 @@
 const debug = require("debug")("ReduxList:useList")
 
-import { useListSelector } from "./use-list-selector"
+import { pipe, get, hasWith, findWith, isNotEmpty, is } from "@mutantlove/m"
 
 /**
  * Custom hook for easy interfacing Redux List
@@ -14,7 +14,37 @@ const useList = (list, dispatch) => {
   list.setDispatch(dispatch)
 
   return {
-    selector: useListSelector(list.name),
+    selector: state => ({
+      head: () => get([list.name, "items", 0])(state),
+
+      byId: (id, notFoundDefault) =>
+        pipe(
+          get([list.name, "items"]),
+          findWith({ id }, notFoundDefault)
+        )(state),
+
+      items: () => get([list.name, "items"])(state),
+      creating: () => get([list.name, "creating"])(state),
+      updating: () => get([list.name, "updating"])(state),
+      removing: () => get([list.name, "removing"])(state),
+      error: action => get([list.name, "errors", action])(state),
+
+      hasWithId: id => pipe(get([list.name, "items"]), hasWith({ id }))(state),
+      isCreating: () => pipe(get([list.name, "creating"]), isNotEmpty)(state),
+      isRemoving: id => {
+        const removing = get([list.name, "removing"])(state)
+
+        return is(id) ? hasWith({ id })(removing) : isNotEmpty(removing)
+      },
+      isUpdating: id => {
+        const updating = get([list.name, "updating"])(state)
+
+        return is(id) ? hasWith({ id })(updating) : isNotEmpty(updating)
+      },
+      isLoading: () => get([list.name, "isLoading"])(state),
+      isLoaded: () => pipe(get([list.name, "loadDate"]), is)(state),
+    }),
+
     create: list.create,
     read: list.read,
     readOne: list.readOne,
